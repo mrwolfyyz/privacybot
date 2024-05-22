515825ef5545fc310232162c67ede74be94963cd
mrwolfyyz <mrwolfbrad@gmail.com>
2024-05-22 10:39:26 -0500

    Update basic_memory.py
    Swapped out ChatGPT for llama3 on groq

--- basic_memory.py
+++ basic_memory.py
@@ -28,25 +28,51 @@ if len(msgs.messages) == 0:
 view_messages = st.expander("View the message contents in session state")
 
 # Get an OpenAI API Key before continuing
-if "openai_api_key" in st.secrets:
-    openai_api_key = st.secrets.openai_api_key
+if "groq_api_key" in st.secrets:
+    groq_api_key = st.secrets.groq_api_key
 else:
-    openai_api_key = st.sidebar.text_input("OpenAI API Key", type="password")
-if not openai_api_key:
-    st.info("Enter an OpenAI API Key to continue")
+    groq_api_key = st.sidebar.text_input("GROQ API Key", type="password")
+if not groq_api_key:
+    st.info("Enter an GROQ API Key to continue")
     st.stop()
 
 # Set up the LangChain, passing in Message History
+sys_msg = """
+As a technical support representative with endless time and patience, your primary goal is to help users enhance their online security posture. 
+Tailor your assistance to match the user's technical expertise, inferred from their questions and interactions. 
+Utilize realistic examples and provide step-by-step guidance to clarify complex concepts.
+
+
+In general, aim to provide detailed responses with practical advice. Use real world  or realistic examples and step-by-step guidance. 
+Provide specific security recommendations based on the user's operating system, when possible.
+If explaining phishing or smishing attacks provide specific examples of the relevant messages and copy for that specific scam. 
+
+When offering security recommendations, personalize them based on the user's operating system. 
+For multi-step processes, offer to guide the user through each step, ensuring they are comfortable proceeding.
+
+Proactively engage with the user to determine if they need further clarification or assistance. 
+When presented with a privacy policy link, simplify the explanation, assume that the user is a five yar old.
+
+
+
+For general inquiries about security posture or risk reduction, directly address the user's question and offer a quick 10-question assessment to provide personalized recommendations.
+Remember to ask questions one at a time during the assessment and provide a final score with tailored recommendations based on the user's responses.
+
+If the user provides a JSON with data breaches, assign a score to each breach where 1= not that severe  and 10 = incredibly severe. 
+Please account for recency and exposed data in assigning a score.
+Offer the user clear recommendations on improving their security and offer more detailed step by step guidance for any of the recommendations
+Please respond with well-formed Json that will pass through langchain's JsonOutput parser without issue
+"""
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system", "You are an AI chatbot having a conversation with a human."),
+        ("system", sys_msg),
         MessagesPlaceholder(variable_name="history"),
         ("human", "{question}"),
     ]
 )
 
-chain = prompt | ChatOpenAI(api_key=openai_api_key)
+llm = ChatGroq(temperature=0, groq_api_key=groq_api_key, model_name="llama3-70b-8192")
 chain_with_history = RunnableWithMessageHistory(
     chain,
     lambda session_id: msgs,
