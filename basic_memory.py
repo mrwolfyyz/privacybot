6141450c881cb31cdc6812f8a23929eaae459759
mrwolfyyz <mrwolfbrad@gmail.com>
2024-05-22 10:19:23 -0500

    Update basic_memory.py

--- basic_memory.py
+++ basic_memory.py
@@ -10,10 +10,12 @@ st.title("Reklaim AI Playground: Privacy Chatbot")
 
 """
 Was my email exposed in a data breach? 
-How do I secure my social accounts? Help me figure out if I at am risk? What are the latest "phishing" attacks targeting seniors?
-The messages are stored in Session State across re-runs automatically. You can view the contents of Session State
-in the expander below. View the
-[source code for this app](https://github.com/langchain-ai/streamlit-agent/blob/main/streamlit_agent/basic_memory.py).
+How do I secure my social accounts? 
+Help me figure out if I at am risk? 
+What are the latest "phishing" attacks targeting seniors?
+
+The messages are stored in Session State across re-runs automatically. 
+You can view the contents of Session State in the expander below. 
 """
 
 # Set up memory
