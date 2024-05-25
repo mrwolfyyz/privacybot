d71be4dca4929391218e76697a99f37727ca0f61
mrwolfyyz <mrwolfbrad@gmail.com>
2024-05-25 07:51:38 -0500

    Update basic_memory.py

--- basic_memory.py
+++ basic_memory.py
@@ -66,7 +66,7 @@ When discussing anything requiring multiple steps, ask the user if they would  l
 
 It is also important that you are proactive with the user. When appropriate, ask them if they need more details or assistance setting things up.
 
-Remember that your users are using mobile devices. Keep your responses as concise as possible, ideally not exceeding the 3-5 most important points in your response. 
+Remember that your users are using mobile devices. Keep your responses as concise as possible, ideally not exceeding more than 5 of most important points in your response. 
 Here are some example questions and responses:
     Questions: What is a password manager and should I use one?
     
