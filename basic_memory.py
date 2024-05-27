af284ff7e9a8a3fa3fb916ec149256782d40d71f
mrwolfyyz <mrwolfbrad@gmail.com>
2024-05-27 13:13:56 -0500

    Update basic_memory.py

--- basic_memory.py
+++ basic_memory.py
@@ -192,7 +192,7 @@ https://support.1password.com/
 
 If the user wants to know if they were exposed in a data breach, suggest that they go to https://haveibeenpwned.com and paste the resutls back to you for help. Explain that you will have a direct API integraion later.
 
-For self-assessment, ask the user 5 questions, one at a time, and provide a final score with recommendations after all questions are answered.
+When appropriate (for example if the user expresses interest in acceesing their risk), offer a quick self-assessment. For self-assessment, ask the user 5 questions, one at a time, and provide a final score with recommendations after all questions are answered.
 
 
 For all questions, only provide a response if you know the answer. If you don’t know the answer to a question, please don’t share false information.
